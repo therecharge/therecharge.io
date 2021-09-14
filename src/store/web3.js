ea60d07ef78cdece973c81ab2969c8101c001295
KimJaekyoon <kjk704@gmail.com>
2021-09-14 18:02:10 +0900

    activated SWAP function

--- src/store/web3.js
+++ src/store/web3.js
@@ -3,7 +3,33 @@
 // 어떤 정보를 알 수 있는가? networkId, account
 // 토스트 띄울 수 있어야 하니까. 토스트 세팅하는 아톰도 있어야 한다.
 // const { atom } = require("recoil");
+import { async } from "@aragon/ui/dist/ToastHub";
 import { atom } from "recoil";
+import Web3 from "web3";
+const ERC20_ABI = require("../Component/Desktop/Defi/abis/ERC20ABI.json");
+
+const ETH = new Web3("https://mainnet.infura.io/v3/636c3521d0f648d5b1789cd9388a182f");
+const BNB = new Web3("https://bsc-dataseed.binance.org/");
+const HECO = new Web3("https://http-mainnet.hecochain.com");
+
+let ethInstance = new ETH.eth.Contract(ERC20_ABI, "0xb6Ea0d1C92700b3Dc95dB19183DE95dF76994f37");
+let ethMethods = ethInstance.methods;
+let bepInstance = new BNB.eth.Contract(ERC20_ABI, "0xf88bf2a6af4255EE9862B1aC8891402F6d0952EC");
+let bepMethods = bepInstance.methods;
+let ethPools, bepPools, hecPools
+
+const loadPools = async () => {
+  ethPools = await ethMethods;
+
+  // console.log("Get ethereum pools from contract :", ethMethods);
+
+  console.log(ethPools)
+
+  // console.log("Get binanceChain pools from contract :", bepMethods);
+}
+
+loadPools();
+
 
 export const web3State = atom({
   key: "web3",
