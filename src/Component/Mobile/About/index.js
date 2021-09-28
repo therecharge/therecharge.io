5c08a3fab7478ccff00179dd00420968021fa7d7
KimJaekyoon <kjk704@gmail.com>
2021-09-29 02:50:49 +0900

    changed team-member profile picture

--- src/Component/Mobile/About/index.js
+++ src/Component/Mobile/About/index.js
@@ -220,7 +220,7 @@ function About({ t }) {
             >
               <img
                 style={{ width: "169px", height: "238px" }}
-                src="./aboutEthanKang.png"
+                src="./aboutEk.png"
               />
             </div>
             <div className="member">
