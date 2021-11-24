593fa8b6bd7d20d0c3d49c076ddd1ea1194a8093
toddheo <todd.heo@gmail.com>
2021-11-24 16:02:08 +0900

    fixed number of chargers activated

--- src/Defi/desktop.js
+++ src/Defi/desktop.js
@@ -619,9 +619,10 @@ function Defi({ toast, t }) {
                 </div>
                 <div className="item">
                   <div className="title Roboto_16pt_Bold">
-                    {analytics.numberOf.Charger
+                    {/* {analytics.numberOf.Charger
                       ? analytics.numberOf.Charger
-                      : 0}
+                      : 0} */}
+                    11
                   </div>
                   <div className="text Roboto_12pt_Regular_L_Gray">
                     Number of Chargers Activated
--- src/Defi/mobile.js
+++ src/Defi/mobile.js
@@ -643,9 +643,10 @@ function Defi({ toast, t }) {
                     className="title Roboto_25pt_Black"
                     style={{ textAlign: "center" }}
                   >
-                    {analytics.numberOf.Charger
+                    {/* {analytics.numberOf.Charger
                       ? analytics.numberOf.Charger
-                      : 0}
+                      : 0} */}
+                    11
                   </div>
                   <div className="text Roboto_20pt_Regular_Gray">
                     Number of Chargers{<br />}Activated
