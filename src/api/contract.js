061a800d6f50b123125e30b184117557d5b45e97
wmc15156 <wmc15156@naver.com>
2022-11-03 18:00:04 +0900

    update
    --- COMMIT END ---

--- src/api/contract.js
+++ src/api/contract.js
@@ -1,21 +1,35 @@
-import { APICore } from "./index";
-import axios from "axios";
+import { APICore } from './index';
+import axios from 'axios';
 
 const api = new APICore();
 
 export const getAllContracts = async () => {
-  return await api.get("/blockchain/contracts?enabled=true").then((resp) => resp.data.data);
+  return await api.get('/blockchain/contracts?enabled=true').then((resp) => resp.data.data);
 };
 
 export const getCoingecko = async () => {
-  return await axios.get('https://api.coingecko.com/api/v3/coins/recharge').then(resp => resp.data.market_data.current_price.usd);
-}
+  return await axios
+    .get('https://api.coingecko.com/api/v3/coins/recharge')
+    .then((resp) => resp.data.market_data.current_price.usd)
+    .catch((err) => {
+      return 0;
+    });
+};
 
 export const getPenUsd = async () => {
-  return await axios.get('https://api.coingecko.com/api/v3/coins/protocon').then(resp => resp.data.market_data.current_price.usd)
-}
+  return await axios
+    .get('https://api.coingecko.com/api/v3/coins/protocon')
+    .then((resp) => resp.data.market_data.current_price.usd)
+    .catch((err) => {
+      return 0;
+    });
+};
 
 export const getAssaplayUsd = async () => {
-  return await axios.get('https://api.coingecko.com/api/v3/coins/assaplay').then(resp => resp.data.market_data.current_price.usd);
-}
-
+  return await axios
+    .get('https://api.coingecko.com/api/v3/coins/assaplay')
+    .then((resp) => resp.data.market_data.current_price.usd)
+    .catch((err) => {
+      return 0;
+    });
+};
