28a3f1b89d07e710932645d35275538c99f7deb3
y <hungryoon.y@gmail.com>
2023-05-01 15:06:03 +0900

    이미지 사이즈 예외처리

--- src/Defi/Station/components/List/row.js
+++ src/Defi/Station/components/List/row.js
@@ -636,10 +636,16 @@ function Name({ status, name, info, isLP, isLocked, allContractInfo }) {
           }
           style={
             window.innerWidth > 1088
-              ? name.includes('LP') || info.name === '4.3 MAP Locked' || info.name === '3.31 CCHG Locked'
+              ? name.includes('LP') ||
+                info.name === '4.3 MAP Locked' ||
+                info.name === '5.2 CCHG Locked' ||
+                info.name === '5.2 MAP Locked'
                 ? { width: '70px', height: '40px' }
                 : { width: '40px', height: '40px' }
-              : name.includes('LP') || info.name === '4.3 MAP Locked' || info.name === '3.31 CCHG Locked'
+              : name.includes('LP') ||
+                info.name === '4.3 MAP Locked' ||
+                info.name === '5.2 CCHG Locked' ||
+                info.name === '5.2 MAP Locked'
               ? { width: '88px', height: '50px' }
               : { width: '50px', height: '50px' }
           }
