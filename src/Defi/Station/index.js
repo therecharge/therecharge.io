452897deea8deb828f604775de062ac5c3dddc84
y <hungryoon.y@gmail.com>
2023-06-29 12:18:42 +0900

    activate  private  locker

--- src/Defi/Station/index.js
+++ src/Defi/Station/index.js
@@ -5,9 +5,9 @@ import { useTranslation } from 'react-i18next';
 // import Networks from "./components/Networks";
 // import Slider from "./components/Slider";
 import List from './components/List';
-// import List_Private from "./components/List_Private";
 import Header from './components/Header';
-// import Header_Private from "./components/Header_Private";
+import Header_Private from './components/Header_Private';
+import List_Private from './components/List_Private';
 function Station(props) {
   const [t] = useTranslation();
   const [params, setParams] = useState({
@@ -41,13 +41,8 @@ function Station(props) {
         {/* <Networks setNetwork={setNetwork} network={network} /> */}
         {/* <Slider setParams={setParams} params={params} /> */}
         <List params={params} toast={props.toast} network={network} setTvl={setTvl} />
-        {/* <Header_Private />
-        <List_Private
-          params={params}
-          toast={props.toast}
-          network={network}
-          setPrivateTvl={setPrivateTvl}
-        /> */}
+        <Header_Private />
+        <List_Private params={params} toast={props.toast} network={network} setPrivateTvl={setPrivateTvl} />
       </Content>
     </Container>
   );
